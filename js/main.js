d2cece832318508323b8293dd9d18bb7938409c6
JESUS ALEJANDRO PEINADO AVILA <2020030720@upsin.edu.mx>
2022-10-24 12:47:10 -0600

    Terminacion de la pagina

--- js/main.js
+++ js/main.js
@@ -4,35 +4,52 @@
 
 
 function Calcular(){
+
+  
+    
     let valor = document.getElementById('valor').value;
-    let tabla = document.getElementById('tab').value;
+    let opcion = document.getElementById('opciones').value;
     let enganche = document.getElementById('txtEnganche');
     let financiar = document.getElementById('txtFinanciar');
     let Pago = document.getElementById('txtPago');
+    Limpiar();
 
 
     let enganche2 = valor*0.3;
     let financiar2 = valor-enganche2;
     let pagoMensual
 
-    if(tabla=12){
+    if(opcion==12){
         financiar2 = financiar2 + (financiar2*0.125);
-        pagoMensual=financiar2/12;
-    }else if(tabla=18){
+        pagoMensual=financiar2/opcion;
+    }else if(opcion==18){
         financiar2 = financiar2 + (financiar2*0.172);
-        pagoMensual=financiar2/18;
-    }else if(tabla=24){
+        pagoMensual=financiar2/opcion;
+    }else if(opcion==24){
         financiar2 = financiar2 + (financiar2*0.21);
-        pagoMensual=financiar2/24;
-    }else if(tabla=36){
+        pagoMensual=financiar2/opcion;
+    }else if(opcion==36){
         financiar2 = financiar2 + (financiar2*0.26);
-        pagoMensual=financiar2/36;
-    }else if(tabla=48){
+        pagoMensual=financiar2/opcion;
+    }else if(opcion==48){
         financiar2 = financiar2 + (financiar2*0.45);
-        pagoMensual=financiar2/48;
+        pagoMensual=financiar2/opcion;
     }
 
     enganche.innerHTML=enganche.innerHTML+enganche2;
     financiar.innerHTML=financiar.innerHTML+financiar2;
     Pago.innerHTML=Pago.innerHTML+pagoMensual;
 }
+
+function Limpiar(){
+    let valor = document.getElementById('valor').value;
+    let enganche = document.getElementById('txtEnganche');
+    let financiar = document.getElementById('txtFinanciar');
+    let Pago = document.getElementById('txtPago');
+
+    enganche.innerHTML="";
+    financiar.innerHTML="";
+    Pago.innerHTML="";
+    
+}
+    
